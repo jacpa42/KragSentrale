32abeee649a03476cf45338e063580155e97d46d
jacpa42 <jpctenthoven@gmail.com>
2024-05-17 19:17:49 +0200

    Added final colours

--- Colours/helper.cpp
+++ Colours/helper.cpp
@@ -1,23 +1,24 @@
-#include <cstring>
+#include <cassert>
+#include <chrono>
 #include <fstream>
 #include <iostream>
+#include <random>
 #include <sstream>
-#include <unordered_map>
 #include <vector>
+
 using namespace std;
 using u32 = unsigned int;
+using u8 = unsigned char;
+#define NUM_COLOURS 6
+#define NUM_TYPES 25
 
-vector<u32> parse(const string &path) {
+void parse(const string &path, vector<u32> &matrix, vector<string> &groups) {
   fstream file(path);
-  unordered_map<string, vector<u32>> map;
-  if (!file.is_open()) {
-    cout << "Unable to open " << path << endl;
-    throw;
-  }
+  assert(file.is_open());
 
   string str;
-  u32 counts[5], descriptors[6];
-  u32 none_group_counter = 0;
+  u32 counts[5];
+  bool descriptors[5];
 
   getline(file, str);
   while (getline(file, str)) {
@@ -25,47 +26,78 @@ vector<u32> parse(const string &path) {
     stringstream line_stream(str);
 
     while (getline(line_stream, str, ',') && column <= 7) {
-      if (column++ >= 2)
+      if (column++ >= 2) {
         counts[column - 1] = stoi(str);
-    }
-
-    while (getline(line_stream, str, ',') && column < 13) {
-      if (column++ >= 8)
-        descriptors[column - 8] = stoi(str);
-    }
-    if (!strcmp("NONE", str.c_str())) {
-      str = "SOLO_" + std::to_string(none_group_counter++);
-    }
-    vector<u32> *group_data = nullptr;
-    {
-      auto pair = map.find(str);
-      if (pair != map.end()) {
-        group_data = &pair->second;
-      } else {
-        map.insert({str, vector(25, (u32)0)});
-        group_data = &map[str];
       }
     }
 
-    for (u32 holdtype = 0; holdtype < group_data->size(); holdtype++) {
+    while (getline(line_stream, str, ',') && column <= 12) {
+      if (column++ >= 8) {
+        descriptors[column - 8] = str[0] == '1';
+      }
+    }
+
+    u32 group_data_offset;
+    bool found = false;
+    for (size_t idx = groups.size() - 1; idx < groups.size(); idx--) {
+      if (groups[idx] == str) {
+        found = true;
+        group_data_offset = idx * NUM_TYPES;
+        break;
+      }
+    }
+    if (!found) {
+      groups.push_back(str);
+      group_data_offset = matrix.size();
+      matrix.resize(matrix.size() + 25, 0);
+    }
+
+    for (u32 holdtype = 0; holdtype < 5; holdtype++) {
       for (u32 size = 0; size < 5; size++) {
         if (descriptors[size]) {
-          group_data->data()[5 * holdtype + size] += counts[holdtype];
+          matrix[group_data_offset + 5 * holdtype + size] += counts[holdtype];
         }
       }
     }
   }
+}
 
-  size_t assigned = 0;
-  vector<u32> data(25 * map.size(), (u32)0);
+void generate_colours(vector<u8> &colours) {
+  static random_device rd;
+  static mt19937 eng(rd());
+  static uniform_int_distribution<> distr(0, NUM_COLOURS - 1);
+  for (u8 &x : colours)
+    x = distr(eng);
+}
 
-  for (auto &pair : map) {
-    memcpy(data.data() + assigned, pair.second.data(),
-           pair.second.size() * sizeof(u32));
-    assigned += pair.second.size();
+float score(const vector<u32> &matrix, const vector<u8> &colours,
+            u32 (&count)[NUM_COLOURS * NUM_TYPES], float (&mean)[NUM_TYPES]) {
+  fill(count, count + NUM_COLOURS * NUM_TYPES, 0);
+
+  for (size_t group = 0; group < colours.size(); group++) {
+    size_t col = colours[group];
+    for (size_t type = 0; type < NUM_TYPES; type++) {
+      count[type * NUM_COLOURS + col] += matrix[group * NUM_TYPES + type];
+    }
   }
 
-  cout << "Parse success " << path << " into " << map.size() << " groups"
-       << endl;
-  return data;
+  for (size_t type = 0; type < NUM_TYPES; type++) {
+    mean[type] = 0.0f;
+    for (size_t col = 0; col < NUM_COLOURS; col++) {
+      mean[type] += count[type * NUM_COLOURS + col];
+    }
+    mean[type] /= NUM_COLOURS;
+  }
+
+  float average_stdv = 0.0f;
+  for (size_t type = 0; type < NUM_TYPES; type++) {
+    float var = 0.0f;
+    for (size_t col = 0; col < NUM_COLOURS; col++) {
+      float diff = count[type * NUM_COLOURS + col] - mean[type];
+      var += diff * diff;
+    }
+    average_stdv += std::sqrtf(var);
+  }
+
+  return average_stdv / NUM_TYPES;
 }
--- Colours/solver.cpp
+++ Colours/solver.cpp
@@ -1,14 +1,32 @@
 #include "helper.cpp"
 
-// The goal is to choose the colour distrubution which minimizes the the average
-// variance in the number of holds of a specific type.
-//
-// For example the second row below is better
-//                       |  Red Green Blue | variance
-// Version 0 : Crimps XS |  0   6     7    | 9.888
-// Version 1 : Crimps XS |  4   6     3    | 4.666
+int main(int argc, char *argv[]) {
+  auto timer = chrono::high_resolution_clock();
+  auto start = timer.now();
+  vector<string> groups;
+  vector<u32> data;
 
-int main() {
-  auto cheeta_data = parse("./Cheeta.csv");
-  return 0;
+  parse("./Cheeta.csv", data, groups);
+  parse("./BluePill.csv", data, groups);
+  parse("./XCult.csv", data, groups);
+  parse("./FlatHold.csv", data, groups);
+
+  vector<u8> colours(data.size() / NUM_TYPES);
+  vector<u8> best(colours.size());
+
+  u32 count[NUM_TYPES * NUM_COLOURS];
+  float mean[NUM_TYPES];
+  float best_score = 1e20;
+
+  for (u32 i = 0; i < stof(argv[1]); i++) {
+    generate_colours(colours);
+    float s = score(data, colours, count, mean);
+    if (s < best_score) {
+      best_score = s, colours.swap(best);
+    }
+  }
+
+  for (size_t i = 0; i < groups.size(); i++) {
+    cout << best[i] + 0 << " " << groups[i] << "\n";
+  }
 }
