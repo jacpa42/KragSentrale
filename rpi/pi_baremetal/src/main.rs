f873e561b8548490100025da43b5d08a465fa0ba
jacpa42 <jpctenthoven@gmail.com>
2024-05-29 15:31:42 +0200

    Added baremetal rpi project

---
+++ rpi/pi_baremetal/src/main.rs
@@ -0,0 +1,36 @@
+#![no_std]
+#![no_main]
+
+use core::arch::asm;
+
+mod boot {
+    use core::arch::global_asm;
+    global_asm!(".section .text._start");
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+#[inline]
+fn delay() {
+    unsafe {
+        for _ in 1..50000 {
+            asm!("nop");
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    unsafe {
+        core::ptr::write_volatile(0x3F20_0008 as *mut u32, 1 << 3);
+        loop {
+            core::ptr::write_volatile(0x3F20_001C as *mut u32, 1 << 21);
+            delay();
+            core::ptr::write_volatile(0x3F20_0028 as *mut u32, 1 << 21);
+            delay();
+        }
+    }
+}
